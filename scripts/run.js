c78ecfe3e8a3fd5273dbde29c93ecf760fc09f32
maztak <takuya108817@gmail.com>
2022-05-17 19:26:21 +0900

    initial commit
    with alchemy API setting in hardhat.config.js

---
+++ scripts/run.js
@@ -0,0 +1,35 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
+
+  // test wave
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait();
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
